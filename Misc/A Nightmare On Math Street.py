dde5e9ec822d11cdd7e02702b258e59dd034f1c4
Yassine SABIR <82727031+Yassine-SABIR@users.noreply.github.com>
2024-07-04 23:54:40 +0200

    Add files via upload

---
+++ Misc/A Nightmare On Math Street.py
@@ -0,0 +1,36 @@
+# HTB A Nightmare On Math Street YS4B
+
+import pwn
+import cv2
+import numpy as np
+
+host = "94.237.48.147"
+port = 36327
+
+def equationChanger(equation):
+
+    equation = "(" + equation.replace("*", ")*(") + ")"
+
+    return equation
+
+s = pwn.connect(host, port)
+
+for index in range(500):
+
+    data = s.recvuntil(b" = ?")
+
+    equation = data.split(b"]: ")[1].split(b" = ")[0].decode("utf-8")
+
+    equation = equationChanger(equation)
+
+    result = bytes(str(eval(equation)), "utf-8")
+
+    s.sendline(result)
+
+    print(f"{index} : {equation} = {result}")
+
+
+while True:
+    data = s.recvline()
+    print(data) # HTB{D0_1t_Your$3lf:)}
+
