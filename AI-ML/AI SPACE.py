452b7a17be8be0e375ed0256866bfe95ed1c17f4
Yassine SABIR <82727031+Yassine-SABIR@users.noreply.github.com>
2024-07-04 03:33:57 +0200

    Add files via upload

---
+++ AI-ML/AI SPACE.py
@@ -0,0 +1,20 @@
+# HTB AI SPACE YS4B
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import MDS
+
+# Load the distance matrix
+file_name = "distance_matrix.npy"
+data = np.load(file_name)
+
+# Perform MDS
+mds = MDS(n_components=2, dissimilarity="precomputed", random_state=42)
+pos = mds.fit_transform(data)
+
+# Plot the results
+plt.scatter(pos[:, 0], pos[:, 1], color='blue')
+for i, txt in enumerate(range(len(pos))):
+    plt.annotate(txt, (pos[i, 0], pos[i, 1]))
+plt.grid()
+plt.show()
